1e86514257c1b7a2df39bb4b5ef997c5c750fdad
hasanonik <jh8539615@gmail.com>
2024-06-10 20:20:31 +0600

    added admin and agent profile

--- src/dashboardpages/Agent/AgentProfile.jsx
+++ src/dashboardpages/Agent/AgentProfile.jsx
@@ -1,11 +1,22 @@
 import { Scroll } from "../../components/Scroll";
+import useUser from "../../hooks/useUser";
 
 
 const AgentProfile = () => {
+
+    const [users] = useUser()
+
     return (
         <div className="overflow-x-auto border-2 p-8">
             <Scroll></Scroll>
-            <h1 className='text-3xl font-medium text-black mb-6'>Agent Profile</h1>
+            <h1 className='text-3xl font-medium text-black mb-6'>Profile</h1>
+            <div className="flex items-center gap-5  border-2 w-fit p-5">
+                <img className="rounded-full" src={users.image}></img>
+                <div className="space-y-1">
+                    <p className="text-lg text-black font-medium">Name: {users.name}</p>
+                    <p className="text-lg text-black font-medium capitalize">Role: {users.role}</p>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
+
+
+const useUser = () => {
+    const { user, loading } = useAuth();
+    const axiosSecure = useAxiosSecure();
+
+    const { data: users = [], isPending} = useQuery({
+        queryKey: [user?.email, 'users'],
+        enabled: !loading,
+        queryFn: async () => {
+            const { data } = await axiosSecure.get(`/user/${user.email}`);
+            return data;
+        }
+    })
+
+    return [users, isPending]
+};
+
+export default useUser;
