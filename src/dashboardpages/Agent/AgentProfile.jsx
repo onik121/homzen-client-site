1e21f9397b2630882e2149adefdf699ab46f4584
hasanonik <jh8539615@gmail.com>
2024-06-10 21:02:33 +0600

    update the agent profile image size

--- src/dashboardpages/Agent/AgentProfile.jsx
+++ src/dashboardpages/Agent/AgentProfile.jsx
@@ -11,7 +11,7 @@ const AgentProfile = () => {
             <Scroll></Scroll>
             <h1 className='text-3xl font-medium text-black mb-6'>Profile</h1>
             <div className="flex items-center gap-5  border-2 w-fit p-5">
-                <img className="rounded-full" src={users.image}></img>
+                <img className="rounded-full max-w-[150px]" src={users.image}></img>
                 <div className="space-y-1">
                     <p className="text-lg text-black font-medium">Name: {users.name}</p>
                     <p className="text-lg text-black font-medium capitalize">Role: {users.role}</p>
